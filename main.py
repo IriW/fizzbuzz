2b559b725a5ea67cc0ce7366379afbd312afce67
Irina <32927392+IriW@users.noreply.github.com>
2021-09-13 08:17:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#Write your code below this row 👇
+
+for nr in range(0,101):
+    nr += 1
+    if nr % 3 == 0 and nr % 5 == 0:
+        print("FizzBuzz")
+    elif nr % 3 == 0:
+        print("Fizz")
+    elif nr % 5 == 0:
+        print("Buzz")
+    else:
+        print(nr)
